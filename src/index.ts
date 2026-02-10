fcce5fab0b66ae9173d44431b8eeca1c661c7815
Ivkin-Alexey <ivkin.alexey@yandex.ru>
2026-02-10 19:13:02 +0300

    feat: implement basic app

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import 'dotenv/config'
+import { Bot, GrammyError, HttpError } from 'grammy'
+
+const BOT_API_TOKEN = process.env.BOT_API_TOKEN
+
+if(!BOT_API_TOKEN) {
+  throw new Error("BOT_API_KEY is not defined")
+}
+
+const bot = new Bot(BOT_API_TOKEN)
+
+bot.command('start', (ctx) =>
+  ctx.reply('Привет! Отправь мне любой текст, и я его повторю.'),
+);
+
+// Ответ на любое сообщение
+bot.on('message:text', (ctx) => {
+  ctx.reply(ctx.message.text);
+});
+
+// Обработка ошибок согласно документации
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
+
+async function startBot() {
+  try {
+    bot.start();
+    console.log('Bot started');
+  } catch (error) {
+    console.error('Error in startBot:', error);
+  }
+}
+
+startBot();
+
+
+ 
