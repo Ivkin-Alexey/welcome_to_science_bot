e4820e641951f972c459c9d5eee51d79a9ac4908
Ivkin-Alexey <ivkin.alexey@yandex.ru>
2026-02-19 00:54:38 +0300

    feat: enhance bot functionality with new commands and update configurations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import { Bot, GrammyError, HttpError } from 'grammy'
+import { stickers, startText, scientistsText } from './constants.js'
+import { startKeyboard, scientistKeyboard } from './inlineKeyboards.js'
 
 const BOT_API_TOKEN = process.env.BOT_API_TOKEN
 
@@ -9,9 +11,16 @@ if(!BOT_API_TOKEN) {
 
 const bot = new Bot(BOT_API_TOKEN)
 
-bot.command('start', (ctx) =>
-  ctx.reply('Привет! Отправь мне любой текст, и я его повторю.'),
-);
+bot.command('start', async (ctx) => {
+  await ctx.replyWithSticker(stickers.hello)
+  await ctx.reply(startText, { reply_markup: startKeyboard })
+});
+
+// Клик по «Научные направления» — показываем вторую клавиатуру
+bot.callbackQuery('start_scientists', async (ctx) => {
+  await ctx.answerCallbackQuery()
+  await ctx.reply(scientistsText, { reply_markup: scientistKeyboard })
+});
 
 // Ответ на любое сообщение
 bot.on('message:text', (ctx) => {
--- src/inlineKeyboards.ts
+++ src/inlineKeyboards.ts
@@ -5,15 +5,15 @@ export const inlineKeyboards = {
 }
 
 export const startKeyboard = new InlineKeyboard()
-  .url("Инструкция", "https://docs.google.com/spreadsheets/d/1nT0_Nm5D2sqPyO3nik1zlUigEx-DSt3abBUzU_GY5Go/edit?gid=0#gid=0")
-  .text("Научные направления", "start_scientists")
-  .url("Оборудование", "https://labspmi.ru/search?department=%5B%22%D0%9D%D0%A6%20%C2%AB%D0%9F%D0%B5%D1%80%D0%B5%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B8%20%D1%80%D0%B5%D1%81%D1%83%D1%80%D1%81%D0%BE%D0%B2%C2%BB%22%5D")
-  .text("СНК \"Технолог\"", "start_technolog");
+  .url("Инструкция", "https://docs.google.com/spreadsheets/d/1nT0_Nm5D2sqPyO3nik1zlUigEx-DSt3abBUzU_GY5Go/edit?gid=0#gid=0").row()
+  .text("Научные направления", "start_scientists").row()
+  .url("Оборудование", "https://labspmi.ru/search?department=%5B%22%D0%9D%D0%A6%20%C2%AB%D0%9F%D0%B5%D1%80%D0%B5%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B8%20%D1%80%D0%B5%D1%81%D1%83%D1%80%D1%81%D0%BE%D0%B2%C2%BB%22%5D").row()
+  .url("СНК \"Технолог\"", "https://vk.com/ssc_technologist")
 
 export const scientistKeyboard = new InlineKeyboard()
-  .text("Углерод", "scientist_carbon")
-  .text("Алюминий", "scientist_aluminium")
-  .text("Кремнегель", "scientists_silicaGel")
-  .text("Катализаторы", "scientists_catalyst")
-  .text("Сапонит", "scientists_saponite")
+  .text("Углерод", "scientist_carbon").row()
+  .text("Алюминий", "scientist_aluminium").row()
+  .text("Кремнегель", "scientists_silicaGel").row()
+  .text("Катализаторы", "scientists_catalyst").row()
+  .text("Сапонит", "scientists_saponite").row()
   .text("Агломерация", "scientists_agglomeration")
